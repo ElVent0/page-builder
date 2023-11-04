e13cac58dd78aad7e73956a2909f5655412cf734
Vas <vasylviter95@gmail.com>
2023-11-04 14:38:44 +0200

    Fix
    Fix: split the code

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,11 @@
 import { FC, useState } from "react";
-import { HexColorPicker } from "react-colorful";
-import { TbColumns3 } from "react-icons/tb";
-import { IoMdColorFilter } from "react-icons/io";
 import { v4 as uuidv4 } from "uuid";
 import { Item } from "../types";
 import toast from "react-hot-toast";
 import { generate } from "random-words";
+import ColumnsFilter from "./ColumnsFilter";
+import ColorsFilter from "./ColorsFilter";
+import GapsFilter from "./GapsFilter";
 
 interface HeaderProps {
   setRows: React.Dispatch<React.SetStateAction<Item[]>>;
@@ -36,113 +36,35 @@ const Header: FC<HeaderProps> = ({ setRows }) => {
     }
   };
 
-  console.log(gap);
-
   return (
     <div className="w-1/4 flex justify-around items-center bg-white py-4 rounded-md relative ml-auto mr-auto shadow-md mb-6">
-      <div className="flex items-center gap-4 relative after:content-[''] after:absolute after:bg-gray-200 after:-top-1 after:-right-3.5 after:w-0.5 after:h-12">
-        <button
-          type="button"
-          className="flex justify-center w-24 gap-1 items-center py-2 rounded-md duration-300 border border-indigo-600 hover:bg-indigo-200"
-          onClick={() => {
-            if (isColors || setIsGap) {
-              setIsColors(false);
-              setIsGap(false);
-            }
-            setIsColumns(!isColumns);
-          }}
-        >
-          {numberOfColumns ? (
-            <p className="text-indigo-600">({numberOfColumns})</p>
-          ) : (
-            <TbColumns3 className="text-indigo-600 text-lg" />
-          )}
-          <p>Columns</p>
-        </button>
-        {isColumns && (
-          <ul className="absolute top-16 -left-4 w-32 bg-white py-3 px-4 rounded-md z-50 shadow-md">
-            {[...Array(4).keys()].map((item) => (
-              <li
-                key={item}
-                className="flex justify-center  items-center mb-2 last-of-type:mb-0"
-              >
-                <button
-                  type="button"
-                  className="block w-full rounded-md bg-gray-100 py-1 duration-300 hover:bg-indigo-200"
-                  onClick={() => {
-                    setIsColumns(false);
-                    setNumberOfColumns(item + 1);
-                  }}
-                >
-                  {item + 1}
-                </button>
-              </li>
-            ))}
-          </ul>
-        )}
-      </div>
-      <div className="flex gap-2 items-center relative after:content-[''] after:absolute after:bg-gray-200 after:-top-1 after:-right-3.5 after:w-0.5 after:h-12">
-        <button
-          type="button"
-          className="flex justify-center gap-1 w-20 items-center py-2 rounded-md duration-300 border border-indigo-600 hover:bg-indigo-200"
-          onClick={() => {
-            if (isColumns || setIsGap) {
-              setIsColumns(false);
-              setIsGap(false);
-            }
-            setIsColors(!isColors);
-          }}
-        >
-          <div
-            className="bg-indigo-400 rounded-md p-1"
-            style={{ backgroundColor: color }}
-          >
-            <IoMdColorFilter className="text-white" />
-          </div>
-          <p>Color</p>
-        </button>
-        {isColors && (
-          <div className="absolute top-16 -left-4 p-3 rounded-md bg-white shadow-md z-50 ">
-            <HexColorPicker
-              color={color}
-              onChange={setColor}
-              className=""
-              style={{ width: "90px", height: "180px" }}
-            />
-          </div>
-        )}
-      </div>
-      <div className="flex gap-2 items-center relative after:content-[''] after:absolute after:bg-gray-200 after:-top-1 after:-right-3.5 after:w-0.5 after:h-12">
-        <button
-          type="button"
-          className="flex justify-center gap-1 w-16 items-center py-2 rounded-md duration-300 border border-indigo-600 hover:bg-indigo-200"
-          onClick={() => {
-            if (isColumns || setIsColors) {
-              setIsColumns(false);
-              setIsColors(false);
-            }
-            setIsGap(!isGap);
-          }}
-        >
-          <p className="text-indigo-600">({gap})</p>
-          <p>Gap</p>
-        </button>
-        {isGap && (
-          <div className="absolute top-16 -left-9 w-32 bg-white py-3 px-4 rounded-md z-50 shadow-md">
-            <input
-              type="range"
-              className="w-full cursor-pointer"
-              min="0"
-              max="20"
-              step="4"
-              value={gap}
-              onChange={(e) => {
-                setGap(Number(e.target.value));
-              }}
-            />
-          </div>
-        )}
-      </div>
+      <ColumnsFilter
+        isColors={isColors}
+        setIsGap={setIsGap}
+        setIsColors={setIsColors}
+        setIsColumns={setIsColumns}
+        isColumns={isColumns}
+        numberOfColumns={numberOfColumns}
+        setNumberOfColumns={setNumberOfColumns}
+      />
+      <ColorsFilter
+        isColumns={isColumns}
+        setIsGap={setIsGap}
+        setIsColumns={setIsColumns}
+        setIsColors={setIsColors}
+        isColors={isColors}
+        color={color}
+        setColor={setColor}
+      />
+      <GapsFilter
+        isColumns={isColumns}
+        setIsColors={setIsColors}
+        setIsColumns={setIsColumns}
+        setIsGap={setIsGap}
+        isGap={isGap}
+        gap={gap}
+        setGap={setGap}
+      />
       <button
         type="button"
         className="py-2 px-5 text-white rounded-md bg-indigo-400 duration-300 hover:bg-indigo-600"
